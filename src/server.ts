b437a9c113acb234e03de659f601642c37aef7b5
Ricardo Kubo <kubodeveloper@gmail.com>
2021-02-27 14:28:50 +0000

    Banco de Dados

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
+import 'reflect-metadata'
 import express, { response } from 'express';
+import "./database";
+import { router } from './routes';
 
 const app = express();
 
@@ -12,18 +15,7 @@ const app = express();
 
  // http://localhost:3333/users
 
- app.get("/",(request,response) => {
-
-    return response.json({message: "Hello word - NLW4" });
- });
-
- // 1 param => Rota(Recurso API)
- // 2 param => request,response
-
- app.post("/",(request,response) => {
-     //Recebeu os dados para salvar
-     return response.json({message:"Os dados foram salvos!"});
- });
-
+app.use(express.json()) ;
+app.use(router)
 
 app.listen(3333, () => console.log("Server is running"));
